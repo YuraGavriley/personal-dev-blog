f5dbe4cb05fa147a06ae9ea9b001a934893cfe9e
Yura <yura.gavriley@gmail.com>
2023-07-31 13:24:45 +0300

    Added project template

---
+++ main_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.StartingPageView.as_view(), name="starting-page"),
+    path("posts", views.AllPostView.as_view(), name="all-posts"),
+    # path("posts/<slug:slug>", views.SinglePostView.as_view(), name="single-post"),
+
+]
---
+++ main_app/views.py
@@ -0,0 +1,33 @@
+from .models import Post
+
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView
+from django.views.generic import TemplateView
+from django.views import View
+from .models import Post
+
+
+class StartingPageView(ListView):
+
+    model = Post
+    template_name = "main_app/index.html"
+    ordering = ["-date"]
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+
+class AllPostView(ListView):
+    model = Post
+    template_name = "main_app/all-posts.html"
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+
+# class SinglePostView(TemplateView):
+#     template_name = "post-detail.html"
